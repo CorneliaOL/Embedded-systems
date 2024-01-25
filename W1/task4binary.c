29e27c7d4c20b3ab0f7a05744f73db7baa4cedcf
rizwanra <rizwanra@student.chalmers.se>
2024-01-25 09:47:32 +0100

    task 4

--- W1/task4binary.c
+++ W1/task4binary.c
@@ -1,20 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char* argv[]){
-    int mask = 1<<7;
-    int value = atoi(argv[1]);
-    printf("%d\n", value);
-    char result[9];
 
 
-    for (int i = 0; i < 8; i++){
-        if((mask & value) == 0){
-            result[i] = '0';
-        } else{
-            result[i] = '1';
+/*Directives for defining identifiers from standard libraries*/
+#include <ctype.h>  /* preprocessor directive: includes validation function */
+#include <stdlib.h> /* preprocessor directive: includes functions library*/
+#include <limits.h> /*preprocessor directive: includes sizeof function from library*/
+#include <stdio.h>  /* preprocessor directive: includes Standard I/O library*/
+#include <string.h> /* preprocessor directive: includes functions*/
+
+void displayBits(unsigned int value); // prototype
+
+// Function to check if a string represents a valid unsigned integer
+int isValidUnsignedInt(const char *str) {   //checks the validity of user input, returns 1 if all are digits
+    while (*str) {
+        if (!isdigit((unsigned char)*str)) { //if-flase condition , in case of any other char than digits
+            return 0;  // Return false (0) if a non-digit character is found
         }
-        mask >>= 1;
+        str++;         // increment to while statement
     }
-    result[8] = '\0';
-    printf("%s\n", result);
+    return 1;  // Return true (1) if only digit characters are found
+}
+
+// Main function in the program, program arguments supported
+int main(int argc, char *argv[]) {      /* Start of - main function*/
+
+    const char *targetString = "-h";        /* Variable initial: '-h' as string value to show help message */
+
+    if (argc == 2) {    /* condition: there is exactly one name argument*/
+
+        if (isValidUnsignedInt(argv[1])) {     //implements validity check, and accepts input if all are digits
+            unsigned int x = atoi(argv[1]);        // user input: with casting it from string to unsigned integer
+            displayBits(x);                  // function call by using user input as argument
+        } else if(strcmp(argv[1], targetString) == 0){  //returns the message if user input matches '-h'
+            printf("Help message - Please enter program name (space) valid decimal digits");
+        } else {                                    //returns message in case of invalid user input
+            printf("Error: Invalid input: Please enter a valid unsigned integer.\n");
+            return 0;                               //returns 1 in se of invalid user input
+        }               // end of nested if statement
+
+    } else {            // else block for No arguments
+
+        printf("Error: Empty argument - field can not be empty, please enter valid input integer.\n");
+        return 0;       // ends the program with return value 1
+
+    }
+    return 0;                   /* returns control to the OS with integar value 0 in case of normal execution  */
+}                               /* End of - main function*/
+
+
+
+void displayBits(unsigned int input) {
+
+
+    int bitsValue = 0;
+
+    // Calculate the minimum number of bits needed
+    unsigned int tempValue = input;
+    while (tempValue > 0) {
+        bitsValue++;
+        tempValue >>= 1;
+    }
+
+    // Round up to the nearest byte (8, 16, 24, 32, etc.)
+    int bytesNeeded = (bitsValue + 7) / 8; // Add 7 and integer divide by 8
+    int bitsToMask = bytesNeeded * 8;
+
+    // Set the displayMask based on bitsToDisplay
+    unsigned int displayMask = 1 << (bitsToMask - 1);
+
+    // Loop through the bits to display
+    for (int c = 1; c <= bitsToMask; ++c) {
+        putchar(input & displayMask ? '1' : '0');
+        displayMask >>= 1;
+    }
 }
--- W1/task4hex.c
+++ W1/task4hex.c
@@ -1,11 +1,88 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*Directives for defining identifiers from standard libraries*/
 
-int main(int argc, char* argv[]){
-    char hex_table[16]= {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
-                         'C', 'D', 'E', 'F'};
-    char* binStr = "1100";
+#include <stdio.h>  /* preprocessor directive: includes Standard I/O library*/
+#include <ctype.h>  /* preprocessor directive: includes validation function */
+#include <stdlib.h> /* preprocessor directive: includes functions library*/
+#include <string.h> /* preprocessor directive: includes functions*/
 
-    printf("%d", bin);
+#define BYTES 8   /* preprocessor directive: MACRO for defining number of bytes for system. 1 Byte = 8-bit*/
+
+
+void binaryToHex(unsigned int value); // function prototype
+int isValidUnsignedInt(const char *str); // function prototype
+
+
+
+int main(int argc, char *argv[]) {
+
+    const char *targetString = "-h";        /* Variable initial: '-h' as string value to show help message */
+    char binaryString[BYTES*8+1]; // String to store binary input
+
+
+    if( argc != 2 ){
+
+        scanf("%64s", binaryString); // Read binary string
+        unsigned int binValue = strtoul(binaryString, NULL, 2); // Convert string to number
+        binaryToHex(binValue);
+
+    } else {
+
+        if(isValidUnsignedInt(argv[1])){
+
+            unsigned int binValue = strtoul(argv[1], NULL, 2); // Convert string to number
+
+            binaryToHex(binValue);
+
+        } else if(strcmp(argv[1], targetString) == 0){
+
+            printf("Help message - Please enter program name (space) valid decimal digits");
+            return 0;       // exist program with return value 0
+
+        } else {                                    //returns message in case of invalid user input
+            printf("Error: Invalid input: Please enter a valid unsigned integer.\n");
+            return 0;                               //returns 1 in se of invalid user input
+        }               // end of nested if statement
+
+    }
+    return 0;
 
 }
+
+
+//Function to convert from binary to hex value with binary value as input argument
+void binaryToHex(unsigned int value) {
+    char hexString[BYTES*2+1]; // String initialization for 2 hex digits for two groups of 4 bits + null terminator
+    int lastIndex = BYTES*2; //
+
+    hexString[lastIndex--] = '\0'; // null terminator for string
+
+    for (int i = 0; i < (BYTES*2+1); i++) { // for-loop for two groups of 4-bits i.e. 8-bits program
+        // Extract the rightmost 4 bits and map to hex character
+
+        unsigned int fourBits = value & 0xF; // Bit masking for 4-bits groups
+
+        if (fourBits < 10) {        //for-loop for mapping value from 0-9 as Hexadecimal
+            hexString[lastIndex--] = '0' + fourBits; //maps result to range [0-9]
+        } else {                    //else for mapping value greater than 10
+            hexString[lastIndex--] = 'A' + (fourBits - 10); //maps result to range [A-F]
+        }
+        value >>= 4; // right shift to bit mask next 4-bits group
+    }
+    printf("%s\n", hexString); // Print hex string
+}
+
+
+// Function to check if a string represents a valid unsigned integer
+int isValidUnsignedInt(const char *str) {   //checks the validity of user input, returns 1 if all are digits
+    while (*str) {
+
+        if (!isdigit((unsigned char)*str)) { //if-flase condition , in case of any other char than digits
+            return 0;  // Return false (0) if a non-digit character is found
+        }
+
+        str++;         // increment to while statement
+    }
+    return 1;  // Return true (1) if only digit characters are found
+}
+
+
