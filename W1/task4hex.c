d6b1afeb7f6025295d1a2725769491e8b79ca708
rizwanra <rizwanra@student.chalmers.se>
2024-01-25 09:23:50 +0000

    commenting correction v2

--- W1/task4hex.c
+++ W1/task4hex.c
@@ -23,20 +23,20 @@ int main(int argc, char *argv[]) {
 
         scanf("%64s", binaryString); // Read binary string
         unsigned int binValue = strtoul(binaryString, NULL, 2);     // Convert string to number
-        binaryToHex(binValue);
+        binaryToHex(binValue);                                      // function call
 
     } else {
 
-        if(isValidUnsignedInt(argv[1])){
+        if(isValidUnsignedInt(argv[1])){                            // validation function to verify digits in the input value
 
             unsigned int binValue = strtoul(argv[1], NULL, 2);      // Convert string to number
 
-            binaryToHex(binValue);
+            binaryToHex(binValue);                                  // function call
 
-        } else if(strcmp(argv[1], targetString) == 0){
+        } else if(strcmp(argv[1], targetString) == 0){              // matches the argument with the defined string i.e. '-h' in this case
 
             printf("Help message - Please enter program name (space) valid decimal digits");
-            return 0;       // exist program with return value 0
+            return 0;                                               // exist program with return value 0
 
         } else {                                    //returns message in case of invalid user input
             printf("Error: Invalid input: Please enter a valid unsigned integer.\n");
@@ -51,19 +51,19 @@ int main(int argc, char *argv[]) {
 
 //Function to convert from binary to hex value with binary value as input argument
 void binaryToHex(unsigned int value) {
-    char hexString[BYTES*2+1];  // String initialization for 2 hex digits for two groups of 4 bits + null terminator
-    int lastIndex = BYTES*2;    // variable declaration & initialization for the last index
+    char hexString[BYTES*2+1];              // String initialization for 2 hex digits for two groups of 4 bits + null terminator
+    int lastIndex = BYTES*2;                // variable declaration & initialization for the last index
 
-    hexString[lastIndex--] = '\0'; // null terminator for string
+    hexString[lastIndex--] = '\0';          // null terminator for string
 
     for (int i = 0; i < (BYTES*2+1); i++) { // for-loop for iterating over groups of 4-bits i.e. depends on defined Macro
         // Extract the rightmost 4 bits and map to hex character
 
         unsigned int fourBits = value & 0xF; // Bit masking for 4-bits groups
 
-        if (fourBits < 10) {        //for-loop for mapping value from 0-9 as Hexadecimal
-            hexString[lastIndex--] = '0' + fourBits; //maps result to range [0-9]
-        } else {                    //else for mapping value greater than 10
+        if (fourBits < 10) {                //for-loop for mapping value from 0-9 as Hexadecimal
+            hexString[lastIndex--] = '0' + fourBits;        //maps result to range [0-9]
+        } else {                                            //else for mapping value greater than 10
             hexString[lastIndex--] = 'A' + (fourBits - 10); //maps result to range [A-F]
         }
         value >>= 4; // right shift to bit mask next 4-bits group
