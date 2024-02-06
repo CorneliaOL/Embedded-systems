83bb08bd3d217f3cda5022e03ae19379d1753ba9
Lucas Carlsson Holter <holter@chalmers.se>
2024-02-06 13:31:46 +0100

    Refactor

--- W2/task3.c
+++ W2/task3.c
@@ -19,6 +19,7 @@ void search_by_famname(void); // Prints out the person with the coresponding fam
 void append_file(PERSON *inrecord); // appends a new person to the file
 PERSON* createPerson(void);
 void clearBuffer(void);
+int read(char*, int);
 
 #define filename "record.bin"
 
@@ -26,7 +27,7 @@ int main(void){
     PERSON ppost;
     PERSON* pPerson;
     int cont = 1;
-    int menuChoice;
+    char menuChoice;
     char searchBy;
 
     while(cont == 1){
@@ -36,24 +37,24 @@ int main(void){
         printf("4 Print out all in the file.\n");
         printf("5 Exit the program.\n");
 
-        scanf("%2d", &menuChoice);
+        menuChoice = getchar();
         clearBuffer();
 
         switch (menuChoice) {
-            case 1:
+            case '1':
                 pPerson = &ppost;
                 strcpy(pPerson -> firstname, "Test");
                 strcpy(pPerson -> famname, "Testfam");
                 strcpy(pPerson -> pers_number, "0123456789");
                 write_new_file(pPerson);
                 break;
-            case 2:
+            case '2':
                 append_file(createPerson());
                 break;
-            case 3:
+            case '3':
 
                 printf("Search by first name(1) or family name(2)?\n");
-                scanf("%c", &searchBy);
+                searchBy = getchar();
                 clearBuffer();
 
                 if(searchBy == '1'){
@@ -64,10 +65,10 @@ int main(void){
                     printf("Invalid character\n");
                 }
                 break;
-            case 4:
+            case '4':
                 printfile();
                 break;
-            case 5:
+            case '5':
                 cont = 0;
                 break;
             default:
@@ -98,13 +99,16 @@ void printfile(void){
 
 
 void search_by_firstname(void){
-    char* search = malloc(20);
+    char search[20];
     FILE* pFile;
     PERSON found;
 
     printf("Type firstname you want to search:\n");
-    scanf("%19s", search);
-    clearBuffer();
+    // scanf("%19s", search);
+    // clearBuffer();
+    while(read(search, sizeof(search)) != 0){
+        printf("Invalid input, try again:\n");
+    }
 
 
     if((pFile = fopen(filename, "rb")) == NULL){
@@ -127,12 +131,15 @@ void search_by_firstname(void){
 }
 
 void search_by_famname(void){
-    char* search = malloc(20);
+    char search[20];
 
     printf("Type family name you want to search:\n");
-    scanf("%19s", search);
-    clearBuffer();
+    // scanf("%19s", search);
+    // clearBuffer();
 
+    while(read(search, sizeof(search)) != 0){
+        printf("Invalid input, try again:\n");
+    }
 
     FILE* pFile;
 
@@ -154,7 +161,6 @@ void search_by_famname(void){
     }
 
     fclose(pFile);
-    free(search);
 }
 
 
@@ -190,21 +196,55 @@ PERSON* createPerson(void){
     PERSON* pPerson = malloc(sizeof(PERSON));
 
     printf("First name?\n");
-    scanf("%19s", pPerson -> firstname);
-    clearBuffer();
-
+    // scanf("%19s", pPerson -> firstname);
+    // clearBuffer();
+    while(read(pPerson -> firstname, sizeof(pPerson -> firstname)) != 0){
+        printf("Invalid input, try again:\n");
+    }
 
     printf("Family name?\n");
-    scanf("%19s", pPerson -> famname);
-    clearBuffer();
+    // scanf("%19s", pPerson -> famname);
+    // clearBuffer();
+    while(read(pPerson -> famname, sizeof(pPerson -> famname)) != 0){
+        printf("Invalid input, try again:\n");
+    }
 
 
     printf("Personnummber?\n");
-    scanf("%12s", pPerson -> pers_number);
-    clearBuffer();
+    // scanf("%12s", pPerson -> pers_number);
+    // clearBuffer();
+
+    while(read(pPerson -> pers_number, sizeof(pPerson -> pers_number)) != 0){
+        printf("Invalid input, try again:\n");
+    }
 
 
     return pPerson;
+}
+
+int read(char* input, int size){
+    char overFlowFlag = 1;
+    if (fgets(input, size, stdin) == NULL){
+        return -1;
+    }
+
+    char* iterator = input;
+    while(*iterator != '\0'){
+        if(*iterator == '\n'){
+            *iterator = '\0';
+            overFlowFlag = 0;
+        }
+    }
+
+    if(overFlowFlag == 1){
+        clearBuffer();
+    }
+
+    if(*input == '\0'){
+        return -1;
+    }
+
+    return 0;
 }
 
 void clearBuffer(void){
