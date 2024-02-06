a767d8dea1f8a62a74a436880b1fb18c232bfc42
Cornelia Olofsson Larsson <guscornol@student.gu.se>
2024-02-06 13:01:17 +0000

    add submission information

--- W2/decode.c
+++ W2/decode.c
@@ -1,5 +1,8 @@
-//240618 - submission code
 
+// (C) Lucas Holter, Cornelia Olofsson, Rizwan Rafiq, group: 18 (2024)
+// Work package 2
+// Exercise 4b
+// Submission code: 240618
 //Import statements
 #include <stdio.h>
 #include <stdlib.h>
